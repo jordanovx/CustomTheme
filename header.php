c24a3db14ea58301773917f6e59e0f6d9aeb7151
jordanovx <jordanov2295@gmail.com>
2025-03-31 17:06:22 +0200

    Fixing navigation and refactoring head

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <?php wp_head() ?>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <meta name="description"
+         content="JorTech Solutions provides cutting-edge IT solutions, custom software development, and digital services to boost your business success.">
+     <meta name="author" content="">
 
  </head>
 
@@ -24,19 +29,18 @@
      </div>
 
      <!-- Navigation -->
-     <nav class="navbar navbar-expand-lg static-top">
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
          <div class="container">
-             <a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/header-logo.png" alt=""></a>
-             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                 <span class="navbar-toggler-icon"></span>
+             <a class="navbar-brand" href="<?php echo site_url(); ?>">
+                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/header-logo.png" alt="Logo">
+             </a>
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                 <i class="fa fa-bars"></i>
              </button>
              <div class="collapse navbar-collapse" id="navbarResponsive">
                  <ul class="navbar-nav ml-auto">
                      <li class="nav-item active">
-                         <a class="nav-link" href="index.html">Home
-                             <span class="sr-only">(current)</span>
-                         </a>
+                         <a class="nav-link" href="index.html">Home</a>
                      </li>
                      <li class="nav-item">
                          <a class="nav-link" href="products.html">Products</a>
